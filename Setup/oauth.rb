04c2031c33eaa27c6c1da332ae61a68ce2e4eae9
srydos <srydos@gmail.com>
2017-07-13 12:24:33 +0900

    serorin merge

--- Setup/oauth.rb
+++ Setup/oauth.rb
@@ -1,41 +1,42 @@
-#!/usr/bin/env ruby
-WORK_DIR=File.expand_path(__FILE__).sub(/[^\/]+$/,'')
 require 'oauth'
 require 'oauth/consumer'
-require 'YAML'
-if (ARGV.length != 2) then
+require 'fileutils'
+require 'yaml'
+
+if (ARGV.length < 2) then
   puts "require args (consumer_key) (consumer_secret_key)\'"
   exit
 end
-@consumer_key = ARGV[0]
-@consumer_secret = ARGV[1]
-@consumer = OAuth::Consumer.new( @consumer_key.to_s, @consumer_secret.to_s, {
-    :site=>"https://api.twitter.com"
-    })
-@request_token = @consumer.get_request_token
+
+consumer_key = ARGV[0]
+consumer_secret = ARGV[1]
+yaml_name = ARGV[2] || "user"
+
+consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {site: "https://api.twitter.com"})
+request_token = consumer.get_request_token
+
 puts "get pin in this url"
-puts  @request_token.authorize_url
+puts  request_token.authorize_url
 #urlにアクセスしてから
+sleep 5
 print "input PIN : "
 pin = STDIN.gets
-@access_token = @request_token.get_access_token(:oauth_verifier => pin)
+
+access_token = request_token.get_access_token(:oauth_verifier => pin)
+
 key_hash = {
-  'consumer_key'=>		@consumer_key,
-  'consumer_secret'=>		@consumer_secret,
-  'access_token'=>		@access_token.token,
-  'access_token_secret'=>	@access_token.secret
+  consumer_key:       consumer_key,
+  consumer_secret:    consumer_secret,
+  access_token:       access_token.token,
+  access_token_secret:access_token.secret
 }
 
-print 'this client called ... : '
-name = STDIN.gets.chomp
-export_path = "#{WORK_DIR}../Config/#{name}.yml"
-if File.exist?(export_path)
-  print "#{export_path} is already exist! \noverwrite? type \"yes\" :"
-  ans = STDIN.gets.chomp
-  print 'ok. no action.' unless ans.eql?("yes")
-  exit unless ans.eql?("yes")
+# Configディレクトリ準備
+config_path = File.expand_path(__FILE__).sub(/[^\/]+$/,'') + "../Config"
+FileUtils.mkdir_p(config_path) unless File.exist?(config_path)
+
+open("#{config_path}/#{yaml_name}.yml","w+") do |yml|
+  YAML.dump(key_hash, yml)
 end
-open("#{WORK_DIR}../Config/#{name}.yml","w+") do |e|
-  YAML.dump(key_hash,e)
-end
-puts "#{name}.yml exported."
+
+puts "#{File.expand_path(config_path)}/#{yaml_name}.yml"
