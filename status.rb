d68d1f0d1cbfbbf8ffaf86b8b84114103340d10b
srydos <srydos@gmail.com>
2017-07-15 19:02:02 +0900

    status.rb refactoring

--- status.rb
+++ status.rb
@@ -4,27 +4,12 @@ require WORK_DIR + 'Class/TetesolTwitter.rb'
 twitter_user = TetesolTwitter.new(WORK_DIR + 'Config/user.yml')
 
 args = ARGV
-if args.length > 2 then
+if args.length > 2
   puts 'too many args...'
   exit
-end
-if args[0] == nil or args[0] == '' then
+elsif args[0].nil? or args[0].empty?
   print "arg : (reaction_target_id)"
   exit
 end
-#引数によって操作を選択
-func_name = "status"
-#func_name = "status"   if args.delete("-s") or args.delete("-status")
 target_tweet_id = args[0]
-#puts "\"" + func_name + "\" doing..."
-#begin
-  case func_name 
-  when "status"
-    twitter_user.status(target_tweet_id)
-  else
-    puts "hmm... what method?"
-  end
-#rescue
-#  puts 'reaction error!'
-  exit
-#end
+twitter_user.status target_tweet_id
