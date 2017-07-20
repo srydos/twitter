98a6d169d233da56da44110c7a6b6a09c35ce8d4
srydos <srydos@gmail.com>
2017-07-20 19:03:23 +0900

    talking with rubocop about tweet.rb.

--- tweet.rb
+++ tweet.rb
@@ -1,15 +1,17 @@
 #!/usr/bin/env ruby
-WORK_DIR=File.expand_path(__FILE__).sub(/[^\/]+$/,'')
+# frozen_string_literal: true
+
+WORK_DIR = File.expand_path('./', File.dirname(__FILE__))
 require WORK_DIR + 'Class/TetesolTwitter.rb'
-#半角スペース対応
+# 半角スペース対応
 tweet_user = TetesolTwitter.new(WORK_DIR + 'Config/user.yml')
 
 msg = ''
 args = ARGV
-args.each do | text |
+args.each do |text|
   msg += text + ' '
 end
-if msg == '' or args.length == 0
+if args.empty?
   print 'input massage! : '
   msg = STDIN.gets
 end
