764f20f25d60a25926d8f0ac6b1d60a6698d02da
srydos <srydos@gmail.com>
2017-08-01 15:28:58 +0900

    talk with rubocop about reaction.rb.

--- reaction.rb
+++ reaction.rb
@@ -10,38 +10,38 @@ require_relative 'Class/tetesol_twitter'
 twitter_user = TetesolTwitter.new(WORK_DIR + '/Config/user.yml')
 
 args = ARGV
-if args.length > 2 then
+if args.length > 2
   puts 'too many args...'
   exit
 end
-if args[0] == nil or args[0] == '' then
-  print "arg : (reaction_target_id)"
+if args[0].nil? || args[0].empty?
+  print 'arg : (reaction_target_id)'
   exit
 end
-#引数によって操作を選択
-func_name = ""
-func_name = "delete"   if args.delete("-d") or args.delete("-delete"  )
-func_name = "retweet"  if args.delete("-r") or args.delete("-retweet" )
-func_name = "favorite" if args.delete("-f") or args.delete("-favorite")
+# 引数によって操作を選択
+func_name = ''
+func_name = 'delete'   if args.delete('-d') || args.delete('-delete')
+func_name = 'retweet'  if args.delete('-r') || args.delete('-retweet')
+func_name = 'favorite' if args.delete('-f') || args.delete('-favorite')
 target_tweet_id = args[0]
-puts "\"" + func_name + "\" doing..."
+puts '\'' + func_name + '\' doing...'
 begin
   case func_name
-  when "retweet"
+  when 'retweet'
     result = twitter_user.retweet(target_tweet_id)
-  when "favorite"
+  when 'favorite'
     result = twitter_user.favorite(target_tweet_id)
     if result.empty?
       result = twitter_user.unfavorite(target_tweet_id)
-      puts "unfav."
+      puts 'unfav.'
     else
-      puts "fav."
+      puts 'fav.'
     end
-  when "delete"
+  when 'delete'
     result = twitter_user.delete(target_tweet_id)
-    puts "deleted."
+    puts 'deleted.'
   else
-    puts "hmm... what method?"
+    puts 'hmm... what method?'
   end
   puts 'done!'
 rescue
@@ -49,7 +49,7 @@ rescue
   exit
 end
 
-#帰ってきたツイート配列の中身を表示
+# 帰ってきたツイート配列の中身を表示
 result.each do |tweet|
   twitter_user.tweet_print_console(tweet)
 end
