f3a062f1cc32c9fbb73d0cbe85ba3233fcc00b0e
srydos <srydos@gmail.com>
2017-08-01 16:12:23 +0900

    talk with rubocop about tetesol_streaming.rb.

--- Class/tetesol_streaming.rb
+++ Class/tetesol_streaming.rb
@@ -1,22 +1,20 @@
+# frozen_string_literal: true
+
 require 'twitter'
 require 'yaml'
 require 'pp'
+# ててそるでストリーミングを使う時に読み込むクラス
 class TetesolStreaming
   attr_accessor :client
-  def initialize( key_file_path = "" )
-    if not key_file_path.empty? then
-      @key_hash = YAML.load_file( key_file_path )
-      config = {
-        consumer_key:        @key_hash[:consumer_key],
-        consumer_secret:     @key_hash[:consumer_secret],
-        access_token:        @key_hash[:access_token],
-        access_token_secret: @key_hash[:access_token_secret]
-      }
-      @client = Twitter::Streaming::Client.new(config)
-    else
-      puts 'cannot read file_path...'
-      exit
-    end
+  def initialize(key_file_path = '../Config/user.yml')
+    @key_hash = YAML.load_file(key_file_path)
+    config = {
+      consumer_key:        @key_hash[:consumer_key],
+      consumer_secret:     @key_hash[:consumer_secret],
+      access_token:        @key_hash[:access_token],
+      access_token_secret: @key_hash[:access_token_secret]
+    }
+    @client = Twitter::Streaming::Client.new(config)
   end
 
   def user
