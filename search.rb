cf372262bb7e73b812af413f4c7a4318e520acd0
srydos <srydos@gmail.com>
2017-08-01 17:46:00 +0900

    talk with rubocop about search.

--- search.rb
+++ search.rb
@@ -5,11 +5,11 @@ WORK_DIR = File.expand_path('./', File.dirname(__FILE__))
 require_relative 'Class/tetesol_twitter'
 search_user = TetesolTwitter.new(WORK_DIR + '/Config/user.yml')
 
-if (search_user == nil ) then
+if search_user.nil?
   puts 'user.yml is not found...'
   exit
 end
-#最後に取得したツイートid取得
+# 最後に取得したツイートid取得
 args = ARGV
 case args.length
 when 0
@@ -19,29 +19,26 @@ when 1
   @count = 15
   query = args[0]
   begin
-    timeline = search_user.popular_search( query, @count )
+    timeline = search_user.popular_search(query, @count)
   rescue
     puts 'search request error...?'
   end
-  search_user.tweet_to_yaml( timeline )
+  search_user.tweet_to_yaml(timeline)
   exit
 else
   msg = ''
-  if (args.delete("-h")) then
-    msg +='#'
-  end
-  args.each do | text |
+  msg += '#' if args.delete('-h')
+  args.each do |text|
     msg += text + ' '
   end
   msg[/ $/] = ''
   @count = 15
   query = msg.to_s
   begin
-    timeline = search_user.popular_search( query, @count )
+    timeline = search_user.popular_search(query, @count)
   rescue
     puts 'search request error...?'
   end
-  search_user.tweet_print_console( timeline )
-exit
-  search_user.tweet_print_yaml( timeline.to_h )
+  search_user.tweet_print_console(timeline)
+  search_user.tweet_print_yaml(timeline.to_h)
 end
