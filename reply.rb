ca693f55a836ba5e67c7594bcc3fed2643511530
srydos <srydos@gmail.com>
2017-08-01 15:26:22 +0900

    talk with rubocop about reply.rb

--- reply.rb
+++ reply.rb
@@ -8,26 +8,26 @@ msg = ''
 args = ARGV
 target_tweet_id = 1
 
-#引数チェック
+# 引数チェック
 case args.length
 when 0
   puts 'args:(reply target tweet_id)(text)'
   exit
 when 1
-  #reply対象idのバリデーション
+  # reply対象idのバリデーション
   target_tweet_id = args[0].to_i
-  if !target_tweet_id.is_a?(Integer) or target_tweet_id < 1
+  if !target_tweet_id.is_a?(Integer) || target_tweet_id < 1
     puts 'target_tweet_id invalid!'
     exit
   end
-  #replyIDだけが設定されていた場合は標準入力を受け取る
-  print "input massage! : "
-  msg=STDIN.gets
+  # replyIDだけが設定されていた場合は標準入力を受け取る
+  print 'input massage! : '
+  msg = STDIN.gets
 else
   target_tweet_id = args.shift.to_i
-  #半角スペース対応
-  Array(args).each {|text| msg += text + ' '}
-  msg[/ $/]= '' if msg[/ $/]
+  # 半角スペース対応
+  Array(args).each { |text| msg += text + ' ' }
+  msg[/ $/] = '' if msg[/ $/]
 end
 tweet = tweet_user.reply(target_tweet_id, msg)
 tweet_user.tweet_print_console(tweet)
